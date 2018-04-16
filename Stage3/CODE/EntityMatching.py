99de34cee13853f2e6333d5e750f766107886df3
ShoyiRen <ZepingRen@ZepingdeMacBook-Pro.local>
2018-04-16 13:32:04 -0500

    update

--- Stage3/CODE/EntityMatching.py
+++ Stage3/CODE/EntityMatching.py
@@ -15,6 +15,10 @@ D = ob.block_candset(C, 'author', 'author', rem_stop_words = True, overlap_size 
 # Debugging the 
 dbg = em.debug_blocker(D, A, B, output_size=200)
 dbg.head()
+
+# Save Table D
+#em.to_csv_metadata(D, './TableD.csv')
+
 # Sample candidate set of size 300
 S = em.sample_table(D, 300)
 # Gold label
@@ -32,6 +36,12 @@ J['ltable_edition'] = ''
 J['rtable_edition'] = ''
 J['ltable_pages'] = ''
 J['rtable_pages'] = ''
+
+# Save Set I
+#em.to_csv_metadata(I, './TableI.csv')
+# Save Set J
+#em.to_csv_metadata(J, './TableJ.csv')
+
 # Automatic feature generation
 F = em.get_features_for_matching(A, B, validate_inferred_attr_types = False)
 H = em.extract_feature_vecs(I, feature_table=F, attrs_after=['gold_labels'])
@@ -48,12 +58,39 @@ nb = em.NBMatcher(name='NaiveBayes')
 # Select the best matcher
 result = em.select_matcher([dt,rf, svm, ln, lg, nb], table=H, exclude_attrs=['_id', 'ltable_ID', 'rtable_ID', 'gold_labels'], k=5, target_attr='gold_labels', metric_to_select_matcher='f1')
 print(result['cv_stats'])
-matcher = result['selected_matcher']
+best_matcher = result['selected_matcher']
 
 # Evaluate the matcher
 L = em.extract_feature_vecs(J, feature_table=F, attrs_after=['gold_labels'])
 L.fillna(value='NaN', inplace=True)
-matcher.fit(table=H, exclude_attrs=['_id', 'ltable_ID', 'rtable_ID', 'gold_labels'], target_attr='gold_labels')
-predictions = matcher.predict(table=L, exclude_attrs=['_id', 'ltable_ID', 'rtable_ID', 'gold_labels'], target_attr='predicted_labels', append = True)
+best_matcher.fit(table=H, exclude_attrs=['_id', 'ltable_ID', 'rtable_ID', 'gold_labels'], target_attr='gold_labels')
+predictions = best_matcher.predict(table=L, exclude_attrs=['_id', 'ltable_ID', 'rtable_ID', 'gold_labels'], target_attr='predicted_labels', append = True)
 eval_result = em.eval_matches(predictions, 'gold_labels', 'predicted_labels')
+print('----------Best Matcher: ', best_matcher.name, '----------')
 em.print_eval_summary(eval_result)
+
+# Evaluate other matcher
+matchers = [dt, svm, rf, lg, ln, nb]
+matchers_name = ['DecisionTree', 'SVM', 'RandomForest', 'LogisticRegression', 'LinearRegression', 'NaiveBayes']
+for tool, name in zip(matchers, matchers_name):
+    if(name == best_matcher.name):
+        continue;
+    tool.fit(table=H, exclude_attrs=['_id', 'ltable_ID', 'rtable_ID', 'gold_labels'], target_attr='gold_labels')
+    pred = tool.predict(table=L, exclude_attrs=['_id', 'ltable_ID', 'rtable_ID', 'gold_labels','predicted_labels'], target_attr='predicted_labels', append = True)
+    rlt = em.eval_matches(pred, 'gold_labels', 'predicted_labels')
+    print('----------',name,'----------')
+    em.print_eval_summary(rlt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
